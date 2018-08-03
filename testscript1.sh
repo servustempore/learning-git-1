d4ec4dda5102290c38ef93be95fb99cbe09388fd
servustempore <artur.bill@web.de>
2018-08-03 12:32:53 +0200

    added some random interaction

--- testscript1.sh
+++ testscript1.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo $0
+USR_STRING=$1
+DESIRED_STRING="Watup"
+
+if [ -z $USR_STRING ]
+    then
+        printf "USAGE: $0 'STRING'\n"
+        exit 1
+fi
+
+if [ $USR_STRING == $DESIRED_STRING ]
+    then
+        printf "Good, you gave us a $DESIRED_STRING!\n"
+    else
+        printf "Thats not the string We've wanted to see, but at least it's not empty :)\n"
+fi
